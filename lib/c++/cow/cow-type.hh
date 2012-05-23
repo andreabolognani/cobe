692a515e3cfc4d2388b0898f2b3ae64b7c55d4c0
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2012-05-23 18:19:49 +0200

    cow updated

--- lib/c++/cow/cow-type.hh
+++ lib/c++/cow/cow-type.hh
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2012 Giovanni Simoni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 #ifndef COW_TYPE_H
 #define COW_TYPE_H
 
@@ -87,25 +111,57 @@ namespace cow {
             }
 
         protected:
-            TYPE & access_for_change (const TYPE * replace = NULL)
+
+            /** Detach from any copy and get the internal object
+             *
+             * This method must be called from any class extending Cow
+             * order get the internal object for modification.
+             *
+             * If some other instance of Cow is referring to the internal
+             * object a copy is created; otherwise the internal object is
+             * kept.
+             *
+             * @return A reference to the internal object ready for
+             * modification.
+             */
+            TYPE & access_rw ()
             {
-                if (vessel == NULL || vessel->refcount > 1) {
-                    if (vessel) {
-                        if (prev) prev->next = next;
-                        if (next) next->prev = prev;
-                        drop_vessel();
-                    }
-                    vessel = new Vessel(replace ? *replace : TYPE());
-                }
-                return vessel->value;
+                return access_for_change(NULL);
             }
 
-            TYPE & get_inner ()
+            /** Read-only access to the internal object.
+             *
+             * @return A constant reference to the internal object.
+             */
+            const TYPE & access_ro () const
             {
                 return vessel->value;
             }
 
         private:
+            Cow *prev;
+            Cow *next;
+            Vessel *vessel;
+
+            TYPE & access_for_change (const TYPE * replace)
+            {
+                if (vessel == NULL || vessel->refcount > 1) {
+                    Vessel * new_vessel;
+
+                    if (!replace && vessel) {
+                        replace = &vessel->value;
+                    }
+                    new_vessel = new Vessel(replace ? *replace : TYPE());
+                    if (vessel) {
+                        if (prev) prev->next = next;
+                        if (next) next->prev = prev;
+                        drop_vessel();
+                    }
+                    vessel = new_vessel;
+                }
+                return vessel->value;
+            }
+
             void drop_vessel ()
             {
                 if (vessel == NULL) return;
@@ -114,10 +170,6 @@ namespace cow {
                     delete vessel;
                 }
             }
-
-            Cow *prev;
-            Cow *next;
-            Vessel *vessel;
     };
 
 }
--- lib/c++/cow/cow-vector.hh
+++ lib/c++/cow/cow-vector.hh
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2012 Giovanni Simoni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 #ifndef COW_VECTOR_H
 #define COW_VECTOR_H
 
@@ -16,19 +40,17 @@ namespace cow {
         public:
             Vector ()
             {
-                Super::access_for_change().resize(SIZE);
+                Super::access_rw().resize(SIZE);
             }
 
             TYPE & operator[] (size_t pos)
             {
-                Vec &inner = Super::get_inner();
-                std::vector<TYPE> &vec = Super::access_for_change(&inner);
-                return vec[pos];
+                return Super::access_rw()[pos];
             }
 
             const TYPE & operator[] (size_t pos) const
             {
-                return static_cast<const Vec &>(*this)[pos];
+                return Super::access_ro()[pos];
             }
     };
 
